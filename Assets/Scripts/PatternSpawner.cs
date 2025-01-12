765a95054db43983546997db2a4a032154d6a101
Marcel Hendrik Müller <marcel_mueller86@gmx.de>
2025-01-12 16:50:02 +0100

    small changes

--- Assets/Scripts/PatternSpawner.cs
+++ Assets/Scripts/PatternSpawner.cs
@@ -21,7 +21,7 @@ public class PatternSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnPattern(3);
+        SpawnPattern(0);
     }
 
     // Update is called once per frame
