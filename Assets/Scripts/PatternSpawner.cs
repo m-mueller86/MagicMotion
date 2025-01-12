5c08e434e65249308b913af79cb3b04915414acd
Marcel Hendrik Müller <marcel_mueller86@gmx.de>
2025-01-12 13:57:59 +0100

    SpawnManager added

---
+++ Assets/Scripts/PatternSpawner.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] patterns;
+    
+    public void SpawnPattern(int patternIndex)
+    {
+        GameObject patternPrefab = patterns[patternIndex];
+        SpawnPositionSaver spawnPositionSaver = patternPrefab.GetComponent<SpawnPositionSaver>();
+        Instantiate(patternPrefab, spawnPositionSaver.spawnPosition, Quaternion.identity);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnPattern(3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SpawnPositionSaver.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPositionSaver : MonoBehaviour
+{
+    public Vector3 spawnPosition;
+}
