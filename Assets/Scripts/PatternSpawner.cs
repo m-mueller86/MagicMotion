f3b952ee1660c6fc43c4fe6a7b45c9b4b3f6645e
Marcel Hendrik Müller <marcel_mueller86@gmx.de>
2025-01-15 13:21:37 +0100

    spawnPositions adjusted

--- Assets/Scripts/PatternSpawner.cs
+++ Assets/Scripts/PatternSpawner.cs
@@ -6,22 +6,19 @@ public class PatternSpawner : MonoBehaviour
 {
 
     [SerializeField] private GameObject[] patterns;
-    [SerializeField] private GameObject errorDetectionBall;
-    [SerializeField] private GameObject endPoint;
+   
     
     public void SpawnPattern(int patternIndex)
     {
         GameObject patternPrefab = patterns[patternIndex];
         SpawnPositionSaver spawnPositionSaver = patternPrefab.GetComponent<SpawnPositionSaver>();
-        Instantiate(patternPrefab, spawnPositionSaver.spawnPosition, Quaternion.identity);
-        Instantiate(errorDetectionBall, spawnPositionSaver.errorDetectionBallStartPosition, Quaternion.identity);
-        Instantiate(endPoint, spawnPositionSaver.endPointPosition, Quaternion.identity);
+        Instantiate(patternPrefab, spawnPositionSaver.spawnPosition, spawnPositionSaver.spawnRotation);
     }
     
     // Start is called before the first frame update
     void Start()
     {
-        SpawnPattern(0);
+        SpawnPattern(3);
     }
 
     // Update is called once per frame
--- Assets/Scripts/SpawnPositionSaver.cs
+++ Assets/Scripts/SpawnPositionSaver.cs
@@ -5,6 +5,5 @@ using UnityEngine;
 public class SpawnPositionSaver : MonoBehaviour
 {
     public Vector3 spawnPosition;
-    public Vector3 errorDetectionBallStartPosition;
-    public Vector3 endPointPosition;
+    public Quaternion spawnRotation = Quaternion.identity;
 }
