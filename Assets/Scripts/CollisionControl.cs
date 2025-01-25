9a9693bf05283a9ba7f837a6b310936d8e5c3d88
Marcel Hendrik Müller <marcel_mueller86@gmx.de>
2025-01-25 13:07:30 +0100

    simple accuracy calculation added

--- Assets/Scripts/CollisionControl.cs
+++ Assets/Scripts/CollisionControl.cs
@@ -1,15 +1,45 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CollisionControl : MonoBehaviour
 {
+    private int collisionCount = 0;
+    private float collisionTime = 0f; 
+    private bool isColliding = false; 
+
     private void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.CompareTag("Border"))
         {
             Debug.Log("Collision with border");
+            
+            collisionCount++;
+            isColliding = true;
         }
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.CompareTag("Border") && isColliding)
+        {
+            collisionTime += Time.deltaTime;
+        }
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Border"))
+        {
+            Debug.Log("Collision ended");
+            isColliding = false;
+        }
+    }
+
+    private void Update()
+    {
+        // Hier kannst du die Genauigkeit berechnen
+        float accuracy = 100f - (collisionCount * 2f + collisionTime * 10f); 
+        accuracy = Mathf.Clamp(accuracy, 0f, 100f); 
+
+        Debug.Log($"Accuracy: {accuracy}%");
     }
 }
